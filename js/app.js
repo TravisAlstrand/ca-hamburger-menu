cecdb61595692191fe85346c2d759b37eb8ba057
Travis Alstrand <travisalstrand@gmail.com>
2023-12-12 19:30:14 -0800

    finished but couldn't figure way to condense CSS animation code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 const navBtn = document.querySelector("#navBtn");
 const mobileNav = document.querySelector("#mobileNav");
+const hamburgerLines = document.querySelectorAll(".btn-bar");
 
 navBtn.addEventListener("click", () => {
+  // slide menu in/out
   mobileNav.classList.toggle("active");
+  // animate button
+  navBtn.classList.toggle("active");
+  navBtn.classList.toggle("inactive");
+  // update aria-hidden
+  let isHidden = mobileNav.getAttribute("aria-hidden") === "true";
+  mobileNav.setAttribute("aria-hidden", !isHidden);
+  // remove animation
+  setTimeout(() => {
+    hamburgerLines.forEach((bar) => {
+      bar.style.animation = null;
+    });
+  }, 600);
 });
